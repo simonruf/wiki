8025a3997baa3ce161629f6f0f3af893760a4ac3
Lean Schuler <54767281+FormulaRossa@users.noreply.github.com>
2023-03-20 19:47:20 +0100

    update sitemap generator

--- sitemap.py
+++ sitemap.py
@@ -9,6 +9,7 @@ def build_sitemap():
     pattern = re.compile(r'\[(.+)\]\(([^ ]+?)( "(.+)")?\)')
     data = open('docs/_sidebar.md').read()
     sitemap = ""
+    pages = 0
     for match in pattern.finditer(data):
         path = match.group(2)
         file_path = "docs" + match.group(2)
@@ -19,9 +20,11 @@ def build_sitemap():
         if os.path.exists(file_path):
             path = urllib.parse.urljoin(url, path)
             sitemap += f'{path}\n'
+            pages += 1
         else:
             print(f'Warning: {file_path} not found')
     open('docs/sitemap.txt', 'w').write(sitemap)
+    print(f'Created sitemap with {pages} pages.')
 
 
 build_sitemap()
